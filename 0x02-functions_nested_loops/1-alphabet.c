6e164328ca1fe8002ed7602f6cff742d7e668a44
DENNYYESSAR <dennisokeri5@gmail.com>
2023-10-11 11:04:52 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,7 +4,8 @@
  * print_alphabet - Prints the lowercase alphabet.
  *
  * Description:
- * This function prints all the lowercase letters from 'a' to 'z' followed by a newline character.
+ * This function prints all the lowercase letters from 'a' to 'z'
+ * followed by a newline character.
  */
 void print_alphabet(void)
 {
