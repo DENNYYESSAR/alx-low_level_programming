c4e421680fca168622a2bb76e2708b5f2ac4f6bd
DENNYYESSAR <dennisokeri5@gmail.com>
2023-10-12 13:36:06 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,16 +6,21 @@
  */
 void print_number(int n)
 {
-	if (n < 0)
+	if (n == -2147483648)
+	{
+	_putchar('-');
+	_putchar('2');
+	n = 147483648;
+	}
+	else if (n < 0)
 	{
 	_putchar('-');
 	n = -n;
 	}
 
-	if (n / 10 != 0)
+	if (n / 10 > 0)
 	{
 	print_number(n / 10);
 	}
-
-	_putchar((n % 10) + '0');
+	_putchar(n % 10 + '0');
 }
