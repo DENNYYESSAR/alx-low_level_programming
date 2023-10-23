edc3103e3e325a9306c04e0e5dbf75607856ef3d
DENNYYESSAR <dennisokeri5@gmail.com>
2023-10-23 11:29:08 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,7 +5,12 @@
  * @s: The string to search in
  * @c: The character to locate
  *
- * Return: Always 0
+ * Description: This function locates the first occurrence of the character 'c'
+ * in the string 's' and returns a pointer to it. If the character is
+ * not found, it returns 0.
+ *
+ * Return: A pointer to the first occurrence of the character 'c' in the
+ * string 's', or 0 if the character is not found.
  */
 char *_strchr(char *s, char c)
 {
