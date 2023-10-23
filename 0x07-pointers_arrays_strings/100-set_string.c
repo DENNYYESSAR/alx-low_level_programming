767619829d91ff6df635f6c0f0db9bbc97671923
DENNYYESSAR <dennisokeri5@gmail.com>
2023-10-23 12:41:01 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * set_string - entry point
+  *
+  * @s: input
+  * @to: input
+  *
+  * Return: Always 0.
+  */
+void set_string(char **s, char *to)
+
+{
+	*s = to;
+
+}
