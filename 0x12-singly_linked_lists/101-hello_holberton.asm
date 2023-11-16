b5ad5484e8ce4f283240d13934a82e766e53c722
DENNYYESSAR <dennisokeri5@gmail.com>
2023-11-16 10:52:54 -0800

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,21 @@
-section .data
-    hello db "Hello, Holberton", 0
+section .data  ; Data section, initialized variables
+    msg db "Hello, Holberton", 0  ; C string needs a null terminator
+    fmt db "%s", 10, 0  ; The printf format, "\n", '0'
 
-section .text
-    extern puts
+section .text  ; Code section.
+    extern printf  ; The C function to be called
 
-global main
-main:
-    ; Call puts function
-    mov rdi, hello
-    call puts
+    global main  ; The standard GCC entry point
 
-    ; Exit the program
-    mov eax, 0
-    ret
+main:  ; The program label for the entry point
+    push rbp  ; Set up stack frame, must be aligned
+
+    mov rdi, fmt
+    mov rsi, msg
+    mov rax, 0  ; or can be xor rax, rax
+    call printf  ; Call C function
+
+    pop rbp  ; Restore stack
+
+    mov rax, 0  ; Normal, no error, return value
+    ret  ; Return
