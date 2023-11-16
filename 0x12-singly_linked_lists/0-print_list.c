001545fe2aee422eac54af104cead41ba7bb0944
DENNYYESSAR <dennisokeri5@gmail.com>
2023-11-16 09:32:42 -0800

    try

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,18 +9,22 @@
  */
 size_t print_list(const list_t *h)
 {
- size_t s = 0;
+   int count = 0;
 
- while (h)
- {
- if (!h->str)
- printf("[0] (nil)\n");
- else
- printf("[%u] %s\n", h->len, h->str);
- h = h->next;
- s++;
- }
 
- return (s);
+   while (h)
+   {
+       if (h->str == NULL)
+       {
+           printf("[0] (nil)\n");
+       }
+       else
+       {
+       printf("[%d] %s\n", h->len, h->str);
+       }
+       count++;
+       h = h->next;
+   }
+   return (count);
 }
 
