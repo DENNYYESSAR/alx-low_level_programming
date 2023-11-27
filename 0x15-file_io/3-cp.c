605195c2aaecb09a1e1282de7f483c3610216955
DENNYYESSAR <dennisokeri5@gmail.com>
2023-11-27 10:39:07 -0800

    3-cp.c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 1024
+
+/**
+ * error_exit - Exits the program with an error message.
+ * @code: The exit code.
+ * @message: The error message format.
+ * @arg: The argument for the error message.
+ */
+void error_exit(int code, const char *message, const char *arg)
+{
+	dprintf(STDERR_FILENO, message, arg);
+	exit(code);
+}
+
+/**
+ * main - Copies the content of a file to another file.
+ * @argc: The number of command line arguments.
+ * @argv: The array of command line arguments.
+ *
+ * Return: 0 on success, or an error code on failure.
+ */
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to;
+	ssize_t read_bytes, write_bytes;
+	char buffer[BUFFER_SIZE];
+	char fd_from_str[12];
+	char fd_to_str[12];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
+		exit(97);
+	}
+
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR
+			| S_IRGRP | S_IROTH);
+	if (fd_to == -1)
+		error_exit(99, "Error: Can't write to %s\n", argv[2]);
+
+	while ((read_bytes = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		write_bytes = write(fd_to, buffer, read_bytes);
+		if (write_bytes == -1 || write_bytes != read_bytes)
+			error_exit(99, "Error: Can't write to %s\n", argv[2]);
+	}
+
+	if (read_bytes == -1)
+		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+
+	if (close(fd_from) == -1)
+	{
+		sprintf(fd_from_str, "%d", fd_from);
+		error_exit(100, "Error: Can't close fd %s\n", fd_from_str);
+	}
+
+	if (close(fd_to) == -1)
+	{
+		sprintf(fd_to_str, "%d", fd_to);
+		error_exit(100, "Error: Can't close fd %s\n", fd_to_str);
+	}
+
+	return (0);
+}
