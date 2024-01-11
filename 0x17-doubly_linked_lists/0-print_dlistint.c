fb580a5d455198e50633be2f443f50bbc9f8b3a0
DENNYYESSAR <dennisokeri5@gmail.com>
2024-01-11 05:46:03 -0800

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -18,12 +18,12 @@ size_t print_dlistint(const dlistint_t *h)
 	while (h->prev != NULL)
 		h = h->prev;
 
-        while (h != NULL)
-        {
-                printf("%d\n", h->n);
-                h = h->next;
-                count++;
-        }
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
 
-        return (count);
+	return (count);
 }
